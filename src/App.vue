19d16fdfc9c5d135c8689999603d595a645e89ba
Dragonkid1996 <djnewell@btinternet.com>
2020-09-12 13:44:06 +0100

    First Commit, a simple dice roller using vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="dice-roller">
+    <div id="input">
+      <h2>Input</h2>
+      <p>Roll d2 <button v-on:click="roll(2)">Click Me!</button></p>
+      <p>Roll d4 <button v-on:click="roll(4)">Click Me!</button></p>
+      <p>Roll d6 <button v-on:click="roll(6)">Click Me!</button></p>
+      <p>Roll d8 <button v-on:click="roll(8)">Click Me!</button></p>
+      <p>Roll d10 <button v-on:click="roll(10)">Click Me!</button></p>
+      <p>Roll d12 <button v-on:click="roll(12)">Click Me!</button></p>
+      <p>Roll d20 <button v-on:click="roll(20)">Click Me!</button></p>
+      <p>Roll d100 <button v-on:click="roll(100)">Click Me!</button></p>
+    </div>
+    <div id="output">
+      <h2>Output</h2>
+      <p v-for = "(item, index) in response" :key="index">{{item}}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      response: []
+    }
+  },
+  methods: {
+    roll: function(sides) {
+      let result = (Math.floor(1 + (Math.random() * sides)));
+      this.response.push("You rolled a: " + result)
+    }
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    background: black;
+  }
+  #dice-roller {
+    color: cyan;
+  }
+  #input {
+    color: cornflowerblue;
+  }
+  #output {
+    color:hotpink;
+  }
 </style>
